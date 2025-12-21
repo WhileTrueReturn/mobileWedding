e859a32ffddf523b0a275710b25c3a1c58ba1c54
parkminkyu park <parkminkyupark@parkminkyuui-MacBookPro.local>
2025-12-21 12:35:24 +0900

    구글 애드센스 추가

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -115,7 +115,7 @@ const LandingPage: React.FC = () => {
           <ins className="adsbygoogle"
                style={{ display: 'block' }}
                data-ad-client="ca-pub-7718490524225342"
-               data-ad-slot="YOUR_AD_SLOT_ID"
+               data-ad-slot="7402985821"
                data-ad-format="auto"
                data-full-width-responsive="true"></ins>
         </div>
